d7c79e90d5eae2d1cd1a78b6eef44635fd327b99
Cory Rentz <48699510+mrsniffles905@users.noreply.github.com>
2019-04-10 12:11:18 -0400

    Add files via upload

---
+++ CoryRentz.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+void main() {
+
+	{
+		cout << "my name is Cory" << endl;
+		cout << "\n fuck" << endl;
+	}
+	system("pause");
+	system("cls");
+	return;
+}
