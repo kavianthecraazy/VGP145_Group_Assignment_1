66649e9d797ced7c8f06c845da128d08075850b9
Dylan Homer <homerdylan0@gmail.com>
2019-04-10 12:40:14 -0400

    Added Script

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!");
+            Console.WriteLine("My name is");
+            Console.WriteLine("My name is");
+            Console.WriteLine("My name is");
+            Console.WriteLine("Chika Chika Slim Shady (It's actually Dylan)");
+        }
+    }
+}
